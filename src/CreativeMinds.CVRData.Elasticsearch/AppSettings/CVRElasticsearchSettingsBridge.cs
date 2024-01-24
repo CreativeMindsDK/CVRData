8e6cfdc1aa8320c782d43fc2d2bec431db857d25
Steen Fredberg Tøttrup <sft@creativeminds.dk>
2024-01-24 07:44:58 +0100

    Option snapshot to monitor

--- src/CreativeMinds.CVRData.Elasticsearch/AppSettings/CVRElasticsearchSettingsBridge.cs
+++ src/CreativeMinds.CVRData.Elasticsearch/AppSettings/CVRElasticsearchSettingsBridge.cs
@@ -4,14 +4,14 @@ using System;
 namespace CreativeMinds.CVRData.Elasticsearch.AppSettings {
 
 	public class CVRElasticsearchSettingsBridge : ICVRElasticsearchSettings {
-		protected readonly IOptionsSnapshot<CVRElasticsearchSettingsReader> optionsConfig;
+		protected readonly IOptionsMonitor<CVRElasticsearchSettingsReader> optionsConfig;
 
-		public CVRElasticsearchSettingsBridge(IOptionsSnapshot<CVRElasticsearchSettingsReader> optionsConfig) {
+		public CVRElasticsearchSettingsBridge(IOptionsMonitor<CVRElasticsearchSettingsReader> optionsConfig) {
 			this.optionsConfig = optionsConfig ?? throw new ArgumentNullException(nameof(optionsConfig));
 		}
 
-		public String Username => this.optionsConfig.Value.Username;
-		public String Password => this.optionsConfig.Value.Password;
-		public String Endpoint => this.optionsConfig.Value.Endpoint;
+		public String Username => this.optionsConfig.CurrentValue.Username;
+		public String Password => this.optionsConfig.CurrentValue.Password;
+		public String Endpoint => this.optionsConfig.CurrentValue.Endpoint;
 	}
 }
